a803124fb93383a33d74e54f15fbc63122e07942
Kjuby <merser_shim_new@mail.ru>
2021-04-10 22:54:32 +0800

    try to win OrderActivity

--- app/src/main/java/com/example/hund_hunter/OrderCreationActivity.java
+++ app/src/main/java/com/example/hund_hunter/OrderCreationActivity.java
@@ -2,18 +2,54 @@ package com.example.hund_hunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class OrderCreationActivity extends AppCompatActivity {
+    int DIALOG_TIME = 1;
+    int myHour = 14;
+    int myMinute = 35;
+    TextView tvTime;
 
-    @Override
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_creation);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+    }
+
+    public void onclick(View view) {
+        showDialog(DIALOG_TIME);
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_TIME) {
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
+            return tpd;
+        }
+        return super.onCreateDialog(id);
+    }
+
+    TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            myHour = hourOfDay;
+            myMinute = minute;
+            tvTime.setText("Время пропажи " + myHour + " часов " + myMinute + " минут");
+        }
+    };
+
+ /*   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_creation);
 
 
-    }
+
+    }*/
 }
