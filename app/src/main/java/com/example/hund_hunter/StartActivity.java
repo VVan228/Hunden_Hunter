0e28993b5e8a47d4428e3c1b1af0d9d85cb9a211
Dash <rdashak8@gmail.com>
2021-04-10 23:24:27 +0800

    add code button in registr and start

--- app/src/main/java/com/example/hund_hunter/StartActivity.java
+++ app/src/main/java/com/example/hund_hunter/StartActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
@@ -18,19 +22,29 @@ public class StartActivity extends AppCompatActivity {
 
     public void findButtonOnClick(View view){
         regisr("find");
-
     }
 
     public void lostButtonClick(View view){
         regisr("lost");
+
     }
 
     public void regisr(String extra){
-        Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show();
-        Intent reg_act = new Intent(StartActivity.this, RegistrationActivity.class);
-        reg_act.putExtra(extra, true);
-        startActivity(reg_act);
-
+        // пользователь не авторизован
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Intent reg_act = new Intent(StartActivity.this, RegistrationActivity.class);
+            reg_act.putExtra(extra, true);
+            startActivity(reg_act);
+        }
+        else {
+            /*if (extra == "lost") {
+                Snackbar.make(findViewById(R.id.ll_lostForm), "Вы авторизованы", Snackbar.LENGTH_SHORT).show();
+            }
+            else {
+                Snackbar.make(findViewById(R.id.act_with_map), "Вы авторизованы", Snackbar.LENGTH_SHORT).show();
+            }*/
+            Toast.makeText(this, "Карта", Toast.LENGTH_LONG).show();
+        }
     }
 
 
