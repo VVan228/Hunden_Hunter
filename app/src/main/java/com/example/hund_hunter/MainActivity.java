12ea0c649ba1fdfceeaa1d6f13eba6cbc5f120ed
Kjuby <merser_shim_new@mail.ru>
2021-04-10 15:46:52 +0800

    Start activity fix 01

--- app/src/main/java/com/example/hund_hunter/MainActivity.java
+++ app/src/main/java/com/example/hund_hunter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.hund_hunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Context context = MainActivity.this;
+        Class destinationActivity = StartActivity.class;
+        Intent child_act_intent = new Intent(context, destinationActivity);
+        child_act_intent.putExtra(Intent.EXTRA_TEXT, "");
+        startActivity(child_act_intent);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("kjuby","its Work!!!!");
