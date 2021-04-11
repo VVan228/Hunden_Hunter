153391383623702ebf9602ecb70fb9a72aa294ac
Kjuby <merser_shim_new@mail.ru>
2021-04-11 10:47:04 +0800

    SeekerActivity Work

--- app/src/main/java/com/example/hund_hunter/SeekerActivity.java
+++ app/src/main/java/com/example/hund_hunter/SeekerActivity.java
@@ -2,13 +2,61 @@ package com.example.hund_hunter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class SeekerActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SeekerActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private Marker marker;
+    private GoogleMap mMap;
+    private LatLng coords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seeker);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        LatLng myPlace = new LatLng(52.27537, 104.2774);
+        //mMap.addMarker(new MarkerOptions().position(myPlace).title("здесь"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myPlace));
+        CameraPosition cameraPosition = new CameraPosition(myPlace, 23, 0, 15);
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+            /*    mMap.clear();
+                marker = mMap.addMarker(new MarkerOptions().position(latLng).title("здесь"));
+                coords = latLng;*/
+            }
+        });
+    }
+
+    public void onSubmit(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("coords", coords.toString());
+        setResult(1, intent);
+        finish();
     }
 }
