64241be4022477095db9c28b314c55d62f662759
vvan <vovchick2210@gmail.com>
2021-05-02 00:17:07 +0800

    fire db in seeker activity integration

--- app/src/main/java/com/example/hund_hunter/SeekerActivity.java
+++ app/src/main/java/com/example/hund_hunter/SeekerActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,10 +18,12 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,81 +34,101 @@ public class SeekerActivity extends AppCompatActivity implements OnMapReadyCallb
     private Marker marker;
     private GoogleMap mMap;
     private LatLng coords;
+    FireDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seeker);
 
+        db = new FireDB(new String[]{"orders"});
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         //mMap.setOnMarkerClickListener(SeekerActivity.this);
 
-
-        FirebaseDatabase.getInstance().getReference().child("orders").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        //получить метки
+        db.getData(new ChildEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Log.e("firebase", "Error getting data", task.getException());
-                }
-                else {
-                    Map<String, Map<String,String>> map = (Map<String, Map<String,String>>)task.getResult().getValue();
-                    Map<String, Map<String,String>> map2 = new HashMap<>();
-                    for (Map.Entry<String, Map<String,String>> entry : map.entrySet()) {
-                        map2.put(entry.getKey(), (Map<String, String>)entry.getValue());
-                    }
-                    for (Map.Entry<String, Map<String,String>> entry : map2.entrySet()) {
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Order order = snapshot.getValue(Order.class);
+                String coords = order.getCoord();
+                String email = order.getEmail();
+                String price = order.getPrice();
+                String comment = order.getComment();
+                String time = order.getTime();
 
-                        //Log.d("yy", entry.getValue().get("coord"));
-                        createMarker(entry.getValue().get("coord"), entry.getValue().get("email")+"\n"+entry.getValue().get("price"));
+                Map<String, String> markerData = new HashMap<>();
+                markerData.put("coords", order.getCoord());
+                markerData.put("email", order.getEmail());
+                markerData.put("price", order.getPrice());
+                markerData.put("comment", order.getComment());
+                markerData.put("time", order.getTime());
 
-                        //Log.d("yy", "Key = " + entry.getKey() + ", Value = " + entry.getValue().get("email"));
-                    }
-                }
+                JSONObject markerDataJson = new JSONObject(markerData);
+                createMarker(coords, markerDataJson.toString());
+                Log.d("tag4me", markerDataJson.toString());
             }
-        });
 
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        }, new myQuery(db.getRef()).orderBy("time"));
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //слушатель нажатий на маркеры
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
                 //Toast.makeText(SeekerActivity.this, marker.getPosition().toString(), Toast.LENGTH_LONG).show();
+                JSONObject markerData = null;
+                try {
+                    markerData = new JSONObject(marker.getTag().toString());
+                } catch (JSONException e) {
+                    Map<String, String> info = new HashMap<>();
+                    info.put("information", e.getMessage());
+                    markerData = new JSONObject(info);
+                }
                 TextView text = findViewById(R.id.bottom_sheet_2);
-                text.setText("email: " + marker.getTag().toString().split("\n")[0]);
+                try {
+                    text.setText(markerData.getString("email"));
+                } catch (JSONException e) {
+                    text.setText("error " + e.getMessage());
+                }
 
                 TextView text2 = findViewById(R.id.bottom_sheet_3);
-                text2.setText("reward: " + marker.getTag().toString().split("\n")[1]);
+                text2.setText(markerData.toString());
                 return true;
             }
         });
-        // Add a marker in Sydney and move the camera
+        //Add a marker in Sydney and move the camera
         //LatLng sydney = new LatLng(-34, 151);
         LatLng myPlace = new LatLng(52.27537, 104.2774);
-        //mMap.addMarker(new MarkerOptions().position(myPlace).title("здесь"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(myPlace));
         CameraPosition cameraPosition = new CameraPosition(myPlace, 23, 0, 15);
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-
-                TextView textView=(TextView) findViewById(R.id.bottom_sheet_3);
-                /*textView = data time + discription + etc */
-
-            }
-        });
     }
-
-
-
 
     public void onSubmit(View view) {
         Intent intent = new Intent();
@@ -114,17 +137,7 @@ public class SeekerActivity extends AppCompatActivity implements OnMapReadyCallb
         finish();
     }
 
-
     public void createMarker(String string, String tag){
-        /*String res = string.replaceAll("[^0-9,]","");
-        String[] latlong =  res.split(",");
-        double latitude = Double.parseDouble(latlong[0]);
-        double longitude = Double.parseDouble(latlong[1]);
-
-        LatLng location = new LatLng(latitude, longitude);
-        Log.d("yy", location.toString());
-
-        mMap.addMarker(new MarkerOptions().position(location));*/
         String from_lat_lng = "";
         Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(string);
         while(m.find()) {
@@ -135,7 +148,5 @@ public class SeekerActivity extends AppCompatActivity implements OnMapReadyCallb
         double lon = Double.parseDouble(gpsVal[1]);
         Marker a = mMap.addMarker(new MarkerOptions().zIndex(100).position(new LatLng(lat,lon)));
         a.setTag(tag);
-
-
     }
 }
