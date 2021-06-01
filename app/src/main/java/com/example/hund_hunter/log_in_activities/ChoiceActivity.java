82253ed14652104f779f765cf3aa95a4932b9c1c
rdashk <67552659+rdashk@users.noreply.github.com>
2021-06-01 23:31:33 +0800

    location

--- app/src/main/java/com/example/hund_hunter/log_in_activities/ChoiceActivity.java
+++ app/src/main/java/com/example/hund_hunter/log_in_activities/ChoiceActivity.java
@@ -8,6 +8,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,14 +26,13 @@ import java.util.Date;
 
 public class ChoiceActivity extends AppCompatActivity {
 
-    public final int LOCATION_PERMISSION = 1001;
+    Button find, lose;
 
+    public final int LOCATION_PERMISSION = 1001;
     LocationManager locationManager;
     Location location;
-
     private boolean granted = false;
 
-    //TODO описать listener
     LocationListener listener = new LocationListener() {
         @SuppressLint("DefaultLocale")
         @Override
@@ -47,14 +47,12 @@ public class ChoiceActivity extends AppCompatActivity {
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
             if(provider.equals(LocationManager.NETWORK_PROVIDER))
-                Toast.makeText(getApplicationContext(),
-                        "Status: " + status,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Status: " + status,Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onProviderEnabled(String provider) {
             //что делать, если провайдер включен
-            //прямое воздействие из программного кода запрещено
             if(granted || checkPermission())
                 showLocation(locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER));
         }
@@ -70,15 +68,6 @@ public class ChoiceActivity extends AppCompatActivity {
         if(location == null){
             return;
         }
-        if(location.getProvider().equals(LocationManager.GPS_PROVIDER)){
-            this.location = location;
-            // getLatitude - широта
-            String koord = String.format("%.4f", location.getLatitude());
-            // getLongitude - долгота
-            koord = String.format("%.4f", location.getLongitude());
-            koord = new Date(location.getTime()).toString();
-
-        }
     }
 
     @Override
@@ -88,45 +77,51 @@ public class ChoiceActivity extends AppCompatActivity {
 
         //подключить менеджер местоположения
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        find = (Button) findViewById(R.id.choice_find);
+        lose = (Button) findViewById(R.id.choice_lose);
+
+        find.setOnClickListener(v -> {
+            Intent intent = new Intent(ChoiceActivity.this, SeekerActivity.class);
+            if(location != null) {
+                intent.putExtra("latitude", location.getLatitude());
+                intent.putExtra("longitude", location.getLongitude());
+            }
+            startActivity(intent);
+        });
+
+        lose.setOnClickListener(v -> startActivity(new Intent(ChoiceActivity.this, OrderCreationActivity.class)));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         if (granted || checkPermission()) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    1000 * 10, 20, listener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 20, listener);
             if (locationManager != null) {
                 location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                 if (location != null){
                     // Получить последние координаты
-                    String koord = String.format("%.4f", location.getLatitude());
-                    koord = String.format("%.4f", location.getLongitude());
-                    koord = new Date(location.getTime()).toString();
+                    Log.d("dash.tag",String.format("%.4f", location.getLatitude()));
+                    Log.d("dash.tag",String.format("%.4f", location.getLongitude()));
                 }
             }
         }
     }
 
     private boolean checkPermission(){
-        //относится к опасным разрешениям, требует запроса. Функция вторична - не сразу
-        //объяснить зачем разрешение, запрос только из активностей или фрагментов
+        //относится к опасным разрешениям, требует запроса.
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             //что делать, если разрешение не дано: попробовать запросить повторно
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
             return false;
         }
         return true;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION) {
             granted = true;
@@ -150,29 +145,5 @@ public class ChoiceActivity extends AppCompatActivity {
         locationManager.removeUpdates(listener);
     }
 
-    public void findButtonOnClick(View view){
-        Intent intent = new Intent(ChoiceActivity.this, SeekerActivity.class);
-        if(location != null) {
-            intent.putExtra("latitude", location.getLatitude());
-            intent.putExtra("longitude", location.getLongitude());
 
-        }else{
-            intent.putExtra("latitude", 52.27537);
-            intent.putExtra("longitude", 104.2774);
-        }
-        startActivity(intent);
-    }
-
-    public void lostButtonClick(View view){
-        Intent intent = new Intent(ChoiceActivity.this, OrderCreationActivity.class);
-        if(location != null) {
-            intent.putExtra("latitude", location.getLatitude());
-            intent.putExtra("longitude", location.getLongitude());
-
-        }else{
-            intent.putExtra("latitude", 52.27537);
-            intent.putExtra("longitude", 104.2774);
-        }
-        startActivity(intent);
-    }
 }
